5c09f65a2bdefa9c7bf0c90c614c450db51d2b2f
MigMatos <87149085+MigMatos@users.noreply.github.com>
2023-08-12 20:59:12 -0600

    Add files via upload

---
+++ incl/levels/updateGJDesc.php
@@ -0,0 +1,39 @@
+<?php
+//error_reporting(0);
+chdir(dirname(__FILE__));
+include "../lib/connection.php";
+require_once "../lib/GJPCheck.php";
+$GJPCheck = new GJPCheck();
+require_once "../lib/exploitPatch.php";
+require_once "../lib/mainLib.php";
+require_once "../../config/antiRaidConfig.php";
+$mainLib = new mainLib();
+//here im getting all the data
+$levelDesc = ExploitPatch::remove($_POST["levelDesc"]);
+$levelID = ExploitPatch::remove($_POST["levelID"]);
+if (isset($_POST['udid']) && !empty($_POST['udid'])) {
+	$id = ExploitPatch::remove($_POST["udid"]);
+	if (is_numeric($id)) {
+		exit("-1");
+	}
+} else {
+	$id = GJPCheck::getAccountIDOrDie();
+}
+$rawDesc = str_replace('-', '+', $levelDesc);
+$rawDesc = str_replace('_', '/', $rawDesc);
+$rawDesc = base64_decode($rawDesc);
+if (strpos($rawDesc, '<c') !== false) {
+	$tags = substr_count($rawDesc, '<c');
+	if ($tags > substr_count($rawDesc, '</c>')) {
+		$tags = $tags - substr_count($rawDesc, '</c>');
+		for ($i = 0; $i < $tags; $i++) {
+			$rawDesc .= '</c>';
+		}
+		$levelDesc = str_replace('+', '-', base64_encode($rawDesc));
+		$levelDesc = str_replace('/', '_', $levelDesc);
+	}
+}
+$levelDesc = base64_encode(mb_substr(base64_decode($levelDesc),0,$comment_bytes_limit,'UTF-8')."...");
+$query = $db->prepare("UPDATE levels SET levelDesc=:levelDesc WHERE levelID=:levelID AND extID=:extID");
+$query->execute([':levelID' => $levelID, ':extID' => $id, ':levelDesc' => $levelDesc]);
+echo 1;
