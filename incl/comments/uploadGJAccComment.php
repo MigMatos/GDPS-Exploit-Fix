ea2ebd2d08882744f9d800dc08ff1ed2ef67e8c9
MigMatos <87149085+MigMatos@users.noreply.github.com>
2023-08-12 19:59:56 -0600

    Add files via upload

---
+++ incl/comments/uploadGJAccComment.php
@@ -0,0 +1,31 @@
+<?php
+chdir(dirname(__FILE__));
+//error_reporting(0);
+include "../lib/connection.php";
+require_once "../lib/GJPCheck.php";
+require_once "../lib/exploitPatch.php";
+require_once "../lib/mainLib.php";
+require_once "../lib/commands.php";
+require_once "../../config/antiRaidConfig.php";
+$mainLib = new mainLib();
+$userName = ExploitPatch::remove($_POST["userName"]);
+$comment = ExploitPatch::remove($_POST["comment"]);
+$accountID = GJPCheck::getAccountIDOrDie();
+$userID = $mainLib->getUserID($accountID, $userName);
+$uploadDate = time();
+//usercheck
+if($accountID != "" AND $comment != ""){
+	$decodecomment = base64_decode($comment);
+	if (strlen($decodecomment) >= $comment_bytes_limit){exit("-1");}
+	for ($i=0; $i < count($blocked_words); $i++) { if(strpos(strtolower($decodecomment),strtolower($blocked_words[$i])) !== false){exit("-1");}}
+	if(Commands::doProfileCommands($accountID, $decodecomment)){
+		exit("-1");
+	}
+	$query = $db->prepare("INSERT INTO acccomments (userName, comment, userID, timeStamp)
+										VALUES (:userName, :comment, :userID, :uploadDate)");
+	$query->execute([':userName' => $userName, ':comment' => $comment, ':userID' => $userID, ':uploadDate' => $uploadDate]);
+	echo 1;
+}else{
+	echo -1;
+}
+?>
